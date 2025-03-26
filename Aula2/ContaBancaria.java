b28f218b127c551acccef66f21b83163e1f9154b
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2025-03-26 08:57:15 -0300

    aula_02
    exercicios que fizemos na aula, primeiro contato com java

---
+++ Aula2/ContaBancaria.java
@@ -0,0 +1,19 @@
+
+/**
+ * Escreva uma descrição da classe ContaBancaria aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class ContaBancaria
+{
+    int numc, data;
+    String nome;
+    float saldo;
+    
+}
+
+// variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    /* T´ipos os mesmo do C, menos o char que é: ' '
+     sempre M - String(uma classe) - " "
+       */
---
+++ Aula2_exercicio professora/ContaRestaurante.java
@@ -0,0 +1,20 @@
+
+/**
+ * Escreva uma descrição da classe ContaRestaurante aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class ContaRestaurante
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+     int nrIdentificação, bebidas, sobremesas;
+     float quilos;
+     String tipoPagamento;
+     
+
+     void criarComanda(int ident){
+         nrIdentificação = ident;
+     }
+     
+}
