a144b58430159c41baa37f59123ac15d9ce3780a
AnaPaulaSPin <166036823+AnaPaulaSPin@users.noreply.github.com>
2025-04-07 19:59:16 -0300

    t

---
+++ Shopping/Loja/Loja.java
@@ -0,0 +1,66 @@
+
+public class Loja{
+   private String nome;
+   private String razao;
+   private int numCnpj;
+   private int digCnpj;
+   private float fat;
+   private float area;
+   
+   public void setnome(String nome){
+       this.nome = nome;
+   }
+   
+   public String getnome(){
+       return this.nome;
+    }
+    
+    public void setRazao(String razao){
+       this.razao = razao;
+   }
+   
+   public String getRazao(){
+       return this.razao;
+    }
+    
+    public void setNumCnpj(int numCnpj){
+       this.numCnpj = numCnpj;
+   }
+   
+   public int getNumCnpj(){
+       return this.numCnpj;
+    }
+    
+    public void setDigCnpj(int digCnpj){
+       this.digCnpj = digCnpj;
+   }
+   
+   public int getDigCnpj(){
+       return this.digCnpj;
+    }
+    
+    public void setFat(float fat){
+       this.fat = fat;
+   }
+   
+   public float getFat(){
+       return this.fat;
+    }
+    
+    public void setArea(float area){
+       this.area = area;
+   }
+   
+   public float getArea(){
+       return this.area;
+    }
+    
+   //Construtor
+   public Loja(String nome, String razao, int numCnpj, int dogCnpj, float Fat, float area){
+       
+       
+   }
+}
+
+
+
