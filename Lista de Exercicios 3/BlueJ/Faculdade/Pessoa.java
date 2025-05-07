48c363fc9f976acd795b8856a056dcf39db4d7e3
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2025-05-07 08:58:05 -0300

    aula de herança

---
+++ Lista de Exercicios 3/BlueJ/Faculdade/Pessoa.java
@@ -0,0 +1,27 @@
+
+public class Pessoa
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    protected String aluno;
+
+    /**
+     * Construtor para objetos da classe Pessoa
+     */
+    public Pessoa()
+    {
+        // inicializa variáveis de instância
+       
+    }
+
+    /**
+     * Um exemplo de um método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   um exemplo de um parâmetro de método
+     * @return     a soma de x e y 
+     */
+    public int sampleMethod(int y)
+    {
+        // escreva seu código aqui
+        return y;
+    }
+}
